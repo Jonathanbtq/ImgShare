7eb795f8fd15e3244a9d494f1ce9eab4c79aa2b9
Jonathanbtq <botquin.jonathan@yahoo.fr>
2024-01-03 01:18:51 +0100

    ImgBigFeed

--- src/components/SearshBar.jsx
+++ src/components/SearshBar.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react"
+import pexelsClient from "../services/pexelApi"
+
 export default function SearshBar(){
+    const [photos, setPhotos] = useState([])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const form = e.target
+        const formData = new FormData(form)
+        const query = formData.get('recherche')
+
+        if(query){
+            pexelsClient.photos.search({ query, per_page: 10 })
+                .then((find) => {
+                    setPhotos(find.photos)
+                });
+        }
+        form.reset()
+    }
+
     return (
         <>
             <div className="srh_ctn">
                 <p>Les plus belles photos, images et vidéos libres de droits partagées gratuitement par des créateurs talentueux.</p>
-                <form action="GET">
-                    <input type="text" />
+                <form action="GET" onSubmit={handleSubmit}>
+                    <input type="text"
+                        name="recherche"
+                    />
                     <input type="submit" value="Rechercher" />
                 </form>
             </div>
